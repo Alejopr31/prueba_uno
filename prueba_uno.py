e10ba4a51d7377a17f38a17f6c450b2f0838268f
juaneraso <juangui9508@hotmail.com>
2025-03-27 20:27:56 -0500

    primer commit de Juan

--- prueba_uno.py
+++ prueba_uno.py
@@ -1,2 +1,8 @@
 print("hola mi primerrepositorio en git")
 print ("este es mi segundo cambio en git hub")
+
+numero = 3
+numero_dos = 4 
+
+if  numero > numero_dos:
+    print("Ingrese a tu repositorio")
